7bd2b80a80f0d2c299638620ffd565901385e337
Gavin Leake <gleake@gmail.com>
2014-04-21 09:09:07 +0100

    tidy up and reset method

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
 var myApp = angular.module('myApp', ['ui.bootstrap']);
- 
-/*phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
-});*/
 
-function AlertDemoCtrl($scope) {
-  $scope.alerts = [
-    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
-    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
-  ];
-
-  $scope.addAlert = function() {
-    $scope.alerts.push({msg: "Another alert!"});
-  };
-
-  $scope.closeAlert = function(index) {
-    $scope.alerts.splice(index, 1);
-  };
-
-}
-
-//function PickerCtrl($scope,$http) {
 myApp.controller('PickerCtrl', function ($scope, $http) {
-  $scope.breadcrumbs = [{"link":"q0","title":"Start"}];
   $http.get('options.json').success(function(data) {
-    $scope.q = data['q0'];
     $scope.data = data;
+    $scope.reset();
   });
   $scope.pickOption = function(id,title,idx) {
     $scope.q = $scope.data[id];
@@ -40,5 +11,9 @@ myApp.controller('PickerCtrl', function ($scope, $http) {
     if(idx>-1){
       $scope.breadcrumbs.splice(idx+1);
     }
+  }
+  $scope.reset = function() {
+    $scope.q = $scope.data['q0'];
+    $scope.breadcrumbs = [{"link":"q0","title":"Start"}];
   }
 });
