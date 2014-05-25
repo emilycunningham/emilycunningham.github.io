cd018fc609222dc62d49ea7f51978235c34dfaad
Gavin Leake <gleake@gmail.com>
2014-05-26 00:47:45 +0100

    routing, partials/views, rework options and homes

--- app.js
+++ app.js
@@ -1,22 +1,66 @@
-var myApp = angular.module('myApp', ['ui.bootstrap']);
+angular.module('myApp', ['ngRoute','ngTouch','ui.bootstrap'])
 
-myApp.controller('PickerCtrl', function ($scope, $http) {
+.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+      .when('/questions', {
+        redirectTo: '/questions/q0'
+      })
+      .when('/questions/:qId', {
+        templateUrl: 'questions.html',
+        controller: 'PickerCtrl'
+      })
+      .when('/homes/:homeId', {
+        templateUrl: 'homes.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/homes', {
+        redirectTo: '/homes/0'
+      })
+      .when('/contact', {
+        templateUrl: 'contact.html'
+      })
+      .otherwise({
+        redirectTo: '/questions'
+      });
+  }
+])
+
+.controller('PickerCtrl', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
   $http.get('options.json').success(function(data) {
-    $scope.data = data;
-    $scope.reset();
+    $scope.options = data;
+    //$scope.reset();
+    $scope.q = $scope.options[$routeParams.qId];
   });
   $scope.pickOption = function(id,title,idx) {
-    $scope.q = $scope.data[id];
-    $scope.breadcrumbs.push({"link":id,"title":title});
+    /*$scope.breadcrumbs.push({"link":id,"title":title});
     if(idx>-1){
       $scope.breadcrumbs.splice(idx+1);
+    }*/
+    idx = Object.keys($scope.options).indexOf(id);
+    //console.log(idx);
+    if (idx == -1) {
+      $location.path(id);
+    } else {
+      $location.path("/questions/"+id);
+      //$scope.q = $scope.options[id];
     }
-  }
+  };
   $scope.reset = function() {
-    $scope.q = $scope.data['q'];
-    $scope.breadcrumbs = [{"link":"q","title":"Start"}];
-  }
+    $scope.q = $scope.options['q0'];
+    //$scope.breadcrumbs = [{"link":"q0","title":"Start"}];
+  };
+}])
+
+.controller('HomeCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $http.get('homes.json').success(function(data) {
+    $scope.homes = data;
+    $scope.home = $scope.homes[$routeParams.homeId];
+  });
+  $scope.homeId = function() {
+    return $routeParams.homeId;
+  };
   $scope.checkArray = function(obj) {
     return Array.isArray(obj);
-  }
-});
+  };
+}]);
